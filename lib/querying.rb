34216c2471bb9cac54273d2facba1b2370b7a2ff
Benjamin Small <ben20262@aim.com>
2020-01-25 18:17:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,6 +8,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
+  GROUP BY characters.name
   HAVING MAX(characters.motto);"
 end
 
