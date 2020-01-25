834c94947760061da8068ade7658167518a8f87c
Benjamin Small <ben20262@aim.com>
2020-01-25 18:38:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
   ON series.author_id = authors.id
   JOIN subgenres
   ON series.subgenre_id
-  GROUP BY author.name;"
+  GROUP BY authors.name;"
 end
 
 def select_series_title_with_most_human_characters
