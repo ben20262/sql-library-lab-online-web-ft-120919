7f2c940a05db0b0b1b59c35078731e9448e48f02
Benjamin Small <ben20262@aim.com>
2020-01-25 18:26:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,9 +6,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto, MAX(LENGTH(motto)) AS max_motto
+  "SELECT characters.name, characters.motto
   FROM characters
-  WHERE LENGTH(motto) = max_motto;"
+  HAVING LENGTH(characters.motto) = MAX(LENGTH(characters.motto));"
 end
 
 
